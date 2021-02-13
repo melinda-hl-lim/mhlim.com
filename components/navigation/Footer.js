9339ce76b0c68892ea6799a280158f1b60c083c1
Melinda <melinda.hl.lim@gmail.com>
2021-02-13 12:17:46 -0500

    Remove bridgeapi; fix footer info

--- components/navigation/Footer.js
+++ components/navigation/Footer.js
@@ -8,7 +8,7 @@ export default function Footer() {
         <div className="mt-2 mb-3 sm:w-full sm:px-8 flex justify-center">
           <div className="my-2 mr-6">
             <a
-              href="mailto:awcrotwell@gmail.com"
+              href="mailto:melinda.hl.lim@gmail.com"
               target="_blank"
               rel="noreferrer"
               className="flex flex-col"
@@ -26,7 +26,7 @@ export default function Footer() {
           </div>
           <div className="my-2 mr-6">
             <a
-              href="https://github.com/andrewc910"
+              href="https://github.com/melinda-hl-lim/"
               target="_blank"
               rel="noreferrer"
               className="flex flex-col"
@@ -43,7 +43,7 @@ export default function Footer() {
           </div>
           <div className="my-2 mr-6">
             <a
-              href="https://linkedin/in/awcrotwell"
+              href="https://www.linkedin.com/in/melinda-h-l-lim/"
               target="_blank"
               rel="noreferrer"
               className="flex flex-col"
@@ -82,7 +82,7 @@ export default function Footer() {
           <div className="border-t-2 border-gray-300 flex flex-col items-center">
             <div className="sm:w-2/3 text-center py-6">
               <p className="text-sm text-white font-bold mb-2">
-                Made with ❤️ Andrew Crotwell
+                Made with ❤️ Melinda Lim
               </p>
             </div>
           </div>
--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,6 @@ export default function Home() {
           </title>
         </Head>
         <Hero />
-        <BridgeCaseStudy />
       </Layout>
     </>
   );
