73cab1528984fe522aa31d18370babcff7648d80
Melinda <melinda.hl.lim@gmail.com>
2021-06-04 17:06:49 -0400

    Update Gander case study link

--- components/home/GanderCaseStudy.js
+++ components/home/GanderCaseStudy.js
@@ -44,7 +44,7 @@ export default function GanderCaseStudy() {
 
           <div className="rounded-md shadow max-w-sm mx-auto">
             <a
-              href="https://bridgeapi-dev.github.io/"
+              href="https://gander-framework.github.io/"
               target="_blank"
               rel="noreferrer"
               className="flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
